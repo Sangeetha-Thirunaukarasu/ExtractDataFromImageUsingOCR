9cca140b9da828a59f0fd9646f315b5ab1386abe
Sangeetha Thirunaukarasu <129584372+Sangeetha-Thirunaukarasu@users.noreply.github.com>
2023-07-23 11:58:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import easyocr  # ocr engine to extract data from the image  b
 import pandas as pd
 import numpy as np
 import os
-import cv2
-import matplotlib.pyplot as plt
-import re
+# import cv2
+# import re
 # importing required modules for db connection
-import mysql.connector
-import pymysql
+# import mysql.connector
+# import pymysql
 import sqlalchemy as sal
 from sqlalchemy import text
 
@@ -30,30 +29,33 @@ def db_connection():  # connect mysql db
 
 def create_db():   # create database
     eng = db_connection()
-    #eng.execute('CREATE DATABASE DATA_OCR')
-    eng.execute("show databases")
+    conx = eng.connect()
+    # conx.execute(text('CREATE DATABASE DATA_OCR'))
+    conx.execute(text("show databases"))
 
 
 def db_table_create():   # create table
     eng = db_connection()
-    eng.execute('CREATE TABLE OCR_DATA_TABLE('
-                'ID INT AUTO_INCREMENT PRIMARY KEY,'
-                'COMPANY_NAME VARCHAR(50),'
-                'CARD_HOLDER_NAME VARCHAR(50),'
-                'DESIGNATION VARCHAR(20),'
-                'MOBILE_NUM VARCHAR(20),'
-                'OFFICE_NUMBER VARCHAR(15),'
-                'EMAIL_ADDRESS1 VARCHAR(20),'
-                'EMAIL_ADDRESS2 VARCHAR(20),'
-                'WEBSITE_URL VARCHAR(20),'
-                'AREA VARCHAR(20),'
-                'CITY VARCHAR(20),'
-                'STATE VARCHAR(20),'
-                'PINCODE INT(6) )')
+    conx = eng.connect()
+    # conx.execute(text('CREATE DATABASE DATA_OCR'))
+    conx.execute(text('CREATE TABLE OCR_DATA_TABLE('
+                      'ID INT AUTO_INCREMENT PRIMARY KEY,'
+                      'COMPANY_NAME VARCHAR(50),'
+                      'CARD_HOLDER_NAME VARCHAR(50),'
+                      'DESIGNATION VARCHAR(20),'
+                      'MOBILE_NUM VARCHAR(20),'
+                      'OFFICE_NUMBER VARCHAR(15),'
+                      'EMAIL_ADDRESS1 VARCHAR(20),'
+                      'EMAIL_ADDRESS2 VARCHAR(20),'
+                      'WEBSITE_URL VARCHAR(20),'
+                      'AREA VARCHAR(20),'
+                      'CITY VARCHAR(20),'
+                      'STATE VARCHAR(20),'
+                      'PINCODE INT(6))'))
     return st.success("Table created")
 
 
-tab1, tab2, tab3 = st.tabs(['new', 'view', 'delete'], )
+tab1, tab2, tab3 = st.tabs(['new', 'view', 'delete'])
 
 
 with tab1:
@@ -76,9 +78,7 @@ with tab1:
         save_dir(get_img)
 
     # Image preprocesing - image resizing before fed into OCR engine
-    # reading the image
-
-
+    # reading the imagesdcx
     # def img_prepro(img_pro):
     #     img = cv2.imread(img_pro.name)
     #     img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -141,9 +141,9 @@ with tab1:
         # img_prepro(get_img)
         reader = easyocr.Reader(['en'])
         res = reader.readtext(get_img.name, detail=0, paragraph=False)
-        res = [i.lower() for i in res]
+        res = [val.lower() for val in res]
         if len(res) < 12:
-            for i in range(0, 12-len(res)):
+            for j in range(0, 12-len(res)):
                 res.append('')
         # convert the image to binary data
         img_data = image_to_bin()
