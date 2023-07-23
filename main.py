ea7101d41ce6f2410a2176be7f5ee6f1e1687db7
Sangeetha Thirunaukarasu <129584372+Sangeetha-Thirunaukarasu@users.noreply.github.com>
2023-07-23 17:31:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 # import cv2
 # import re
 # importing required modules for db connection
-# import mysql.connector
-# import pymysql
+import mysql.connector
+import pymysql
 import sqlalchemy as sal
 from sqlalchemy import text
 
@@ -23,39 +23,39 @@ port = '3306'
 
 
 def db_connection():  # connect mysql db
-    return sal.create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'.format(
+    eng = sal.create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'.format(
         user=user, password=pw, host=host, port=port, db_name=db_name))
+    return eng.connect()
 
 
 def create_db():   # create database
     eng = db_connection()
-    conx = eng.connect()
     # conx.execute(text('CREATE DATABASE DATA_OCR'))
-    conx.execute(text("show databases"))
+    eng.execute(text("show databases"))
 
 
 def db_table_create():   # create table
     eng = db_connection()
-    conx = eng.connect()
+    #eng.close()
     # conx.execute(text('CREATE DATABASE DATA_OCR'))
-    conx.execute(text('CREATE TABLE OCR_DATA_TABLE('
-                      'ID INT AUTO_INCREMENT PRIMARY KEY,'
-                      'COMPANY_NAME VARCHAR(50),'
-                      'CARD_HOLDER_NAME VARCHAR(50),'
-                      'DESIGNATION VARCHAR(20),'
-                      'MOBILE_NUM VARCHAR(20),'
-                      'OFFICE_NUMBER VARCHAR(15),'
-                      'EMAIL_ADDRESS1 VARCHAR(20),'
-                      'EMAIL_ADDRESS2 VARCHAR(20),'
-                      'WEBSITE_URL VARCHAR(20),'
-                      'AREA VARCHAR(20),'
-                      'CITY VARCHAR(20),'
-                      'STATE VARCHAR(20),'
-                      'PINCODE INT(6))'))
+    eng.execute(text('CREATE TABLE OCR_DATA_TABLE('
+                     'ID INT AUTO_INCREMENT PRIMARY KEY,'
+                     'COMPANY_NAME VARCHAR(50),'
+                     'CARD_HOLDER_NAME VARCHAR(50),'
+                     'DESIGNATION VARCHAR(20),'
+                     'MOBILE_NUM VARCHAR(20),'
+                     'OFFICE_NUMBER VARCHAR(15),'
+                     'EMAIL_ADDRESS1 VARCHAR(20),'
+                     'EMAIL_ADDRESS2 VARCHAR(20),'
+                     'WEBSITE_URL VARCHAR(20),'
+                     'AREA VARCHAR(20),'
+                     'CITY VARCHAR(20),'
+                     'STATE VARCHAR(20),'
+                     'PINCODE INT(6))'))
     return st.success("Table created")
 
 
-tab1, tab2, tab3 = st.tabs(['new', 'view', 'delete'])
+tab1, tab2, tab3 = st.tabs(['Add NEW', 'View and Update', 'Delete'])
 
 
 with tab1:
@@ -157,20 +157,23 @@ with tab1:
         # st.write(new_res.shape[1])
         st.write("**:green[The extracted information are:]**")
         # convert list into data frame
-        data = pd.DataFrame(new_res, columns=(['CARD_HOLDER_NAME','DESIGNATION','MOBILE_NUM','OFFICE_NUMBER','EMAIL_ADDRESS1','EMAIL_ADDRESS2','WEBSITE_URL','AREA','CITY','STATE','PINCODE','COMPANY_NAME','IMAGE']))
+        db_data = pd.DataFrame(new_res, columns=(['CARD_HOLDER_NAME', 'DESIGNATION', 'MOBILE_NUM', 'OFFICE_NUMBER',
+                                                  'EMAIL_ADDRESS1', 'EMAIL_ADDRESS2', 'WEBSITE_URL', 'AREA', 'CITY',
+                                                  'STATE', 'PINCODE', 'COMPANY_NAME', 'IMAGE']))
         # Editable dataframe
-        final_data = st.data_editor(data, key='data_editor', on_change=call_back(), hide_index=True)
+        final_data = st.data_editor(db_data, key='data_editor', on_change=call_back(), hide_index=True, use_container_width=True)
         save = st.button('Save', key='save')
         st.write("**:red[kindly check all the field has correct values before save]")
         if save:
-            con = db_connection()
+            connection = db_connection()
             # final_data--> dataframe,
             # OCR_DATA_TABLE-->SQL table
             # using to_sql method in DF, converting and insert df to sql table
-            final_data.to_sql(con=con, name='ocr_data_table', if_exists='append', index=False)
+            final_data.to_sql(con=connection, name='ocr_data_table', if_exists='append', index=False)
+            connection.close()
             st.success("Saved Successfully")
 
-    #ocr_reader()
+    # ocr_reader()
 
     try:
         ocr_reader()
@@ -178,16 +181,56 @@ with tab1:
         pass
 
 with tab2:
+    st.write("**Please enter your registered name and designation and press 'Enter'**")
+    name = st.text_input('Enter your name ', key='name1')
+    design = st.text_input('Enter your Designation ', key='design1')
+    #if st.button('View Data', key='view1'):
+    if name != "" and design != "":
+        #try:
+        con = db_connection()
+        # data1 = conn.execute(text('select COMPANY_NAME, CARD_HOLDER_NAME, DESIGNATION, MOBILE_NUM,OFFICE_NUMBER, EMAIL_ADDRESS1, EMAIL_ADDRESS2, WEBSITE_URL, AREA, CITY, STATE, PINCODE from ocr_data_table where CARD_HOLDER_NAME="' + name + '" and DESIGNATION="' + design + '"'))
+        query = 'select * from ocr_data_table where CARD_HOLDER_NAME="' + name.casefold() + '" and DESIGNATION="' + design.casefold() + '"'
+        retrie = con.execute(text(query))
+        ret = list(retrie)
+        #st.write(ret)
+        if ret:
+            st.write("The retrieved information given below:")
+            col_list = ['ID', 'COMPANY_NAME', 'CARD_HOLDER_NAME', 'DESIGNATION', 'MOBILE_NUM', 'OFFICE_NUMBER', 'EMAIL_ADDRESS1',
+                        'EMAIL_ADDRESS2', 'WEBSITE_URL', 'AREA', 'CITY', 'STATE', 'PINCODE']
+            with st.form('my_form'):
+                for da in range(1, len(col_list)):
+                    col_list[da] = st.text_input(label=col_list[da], value=ret[0][da], key=col_list[da])
+                submitted = st.form_submit_button('Save')
+                if submitted:
+                    col_list1 = ['ID', 'COMPANY_NAME', 'CARD_HOLDER_NAME', 'DESIGNATION', 'MOBILE_NUM', 'OFFICE_NUMBER',
+                                 'EMAIL_ADDRESS1', 'EMAIL_ADDRESS2', 'WEBSITE_URL', 'AREA', 'CITY', 'STATE', 'PINCODE']
+                    for col in range(1, len(col_list)):
+                        query_up = 'update ocr_data_table set ' + col_list1[col]+' = ' + '"' + col_list[col] + '"' + ' where ID = {}'.format(ret[0][0])
+                        # st.write(query_up)
+                        con.execute(text(query_up))
+                    st.success("Updated successfully")
+                    con.close()
+        else:
+            st.error("Name or Designation is not registered")
+        #except BaseException as e:
+         #   st.write(e)
+        #else:
+         #   st.warning("Please fill all the fields")
+
+with tab3:
     if 'new_df' not in st.session_state:
         st.session_state.new_df = 0
+
+
     # if 'view' not in st.session_state:
-       # st.session_state['view'] = 0
+    # st.session_state['view'] = 0
 
     def callback1():
         st.session_state['new_df'] += 1
 
+
     # def view_button():
-        # st.session_state.view = 1
+    # st.session_state.view = 1
 
     name = st.text_input('Enter your name ', key='name')
     design = st.text_input('Enter your Designation ', key='design')
@@ -195,33 +238,23 @@ with tab2:
 
     if view:
         con = db_connection()
-        data ='select COMPANY_NAME, CARD_HOLDER_NAME, DESIGNATION, MOBILE_NUM,OFFICE_NUMBER, EMAIL_ADDRESS1, EMAIL_ADDRESS2, WEBSITE_URL, AREA, CITY, STATE, PINCODE from ocr_data_table where CARD_HOLDER_NAME="'+name+'" and DESIGNATION="'+design+'"'
+        data = 'select COMPANY_NAME, CARD_HOLDER_NAME, DESIGNATION, MOBILE_NUM,OFFICE_NUMBER, EMAIL_ADDRESS1, EMAIL_ADDRESS2, WEBSITE_URL, AREA, CITY, STATE, PINCODE from ocr_data_table where CARD_HOLDER_NAME="' + name + '" and DESIGNATION="' + design + '"'
         df1 = pd.read_sql(data, con=con)
-        #st.write(df1)
+        st.write(data)
         # new_df = st.data_editor(df1, num_rows='dynamic', key='new_df') #on_change=callback1())
 
         st.button('Save Changes', key='save_change')
 
-with tab3:
-    name = st.text_input('Enter your name ', key='name1')
-    design = st.text_input('Enter your Designation ', key='design1')
-    #view1 = st.button('View Data', key='view1')
-    con = db_connection()
-    conn = con.connect()
-    #data1 = conn.execute(text('select COMPANY_NAME, CARD_HOLDER_NAME, DESIGNATION, MOBILE_NUM,OFFICE_NUMBER, EMAIL_ADDRESS1, EMAIL_ADDRESS2, WEBSITE_URL, AREA, CITY, STATE, PINCODE from ocr_data_table where CARD_HOLDER_NAME="' + name + '" and DESIGNATION="' + design + '"'))
-    query = 'select * from ocr_data_table where CARD_HOLDER_NAME="' + name + '" and DESIGNATION="' + design + '"'
-    df = pd.read_sql(query, conn)
-    df.pop('IMAGE')
-    d1 = dict(df)
-    new_dat = st.data_editor(df, hide_index=True)
-    #st.write(type(new_dat))
-    d2 = dict(new_dat)
-    #st.write(d)
-    lis = ['COMPANY_NAME', 'CARD_HOLDER_NAME', 'DESIGNATION', 'MOBILE_NUM', 'OFFICE_NUMBER', 'EMAIL_ADDRESS1',
-           'EMAIL_ADDRESS2', 'WEBSITE_URL', 'AREA', 'CITY', 'STATE', 'PINCODE']
-    for i in lis:
-        if d1.values()[0] != d2.values()[0]:
-            query = f'update ocr_data_table set {i} = "{d2[i].values[0]}" where ID = {d2["ID"].values[0]}'
-            #st.write(query)
-            conn.execute(text(query))
-    st.success('updated successfully')
+    #df = pd.read_sql(query, conn)
+    # d1 = dict(df)
+    # new_dat = st.data_editor(df, hide_index=True)
+    # # st.write(type(new_dat))
+    # d2 = dict(new_dat)
+    # # st.write(d)
+    # lis = ['COMPANY_NAME', 'CARD_HOLDER_NAME', 'DESIGNATION', 'MOBILE_NUM', 'OFFICE_NUMBER', 'EMAIL_ADDRESS1',
+    #        'EMAIL_ADDRESS2', 'WEBSITE_URL', 'AREA', 'CITY', 'STATE', 'PINCODE']
+    # for i in lis:
+    #     query = f'update ocr_data_table set {i} = "{d2[i].values[0]}" where ID = {d2["ID"].values[0]}'
+    #     #st.write(query)
+    #     conn.execute(text(query))
+    # st.success('updated successfully')
